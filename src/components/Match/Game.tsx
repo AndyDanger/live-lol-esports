38aa46b2712f4d32ff5dec3d8f1f72518a45a53f
Andrew Marshall <andydanger12@outlook.com>
2024-07-28 08:02:16 -0500

    Attempt to fix teams backwards issue.

--- src/components/Match/Game.tsx
+++ src/components/Match/Game.tsx
@@ -117,7 +117,7 @@ export function Game({ firstWindowFrame, lastWindowFrame, lastDetailsFrame, game
     */
     const summonerName = gameMetadata.blueTeamMetadata.participantMetadata[0].summonerName.split(" ");
 
-    if ((summonerName[0] && redTeam.code.startsWith(summonerName[0])) || gameMetadata.blueTeamMetadata.esportsTeamId !== blueTeam.id) { // Temos que verificar apenas os primeiros caracteres pois os times academy usam o A, a mais na tag mas não nos nomes
+    if ((summonerName[0] && summonerName[0].startsWith(redTeam.code)) || gameMetadata.blueTeamMetadata.esportsTeamId !== blueTeam.id) { // Temos que verificar apenas os primeiros caracteres pois os times academy usam o A, a mais na tag mas não nos nomes
         blueTeam = redTeam;
         redTeam = auxBlueTeam;
     }
