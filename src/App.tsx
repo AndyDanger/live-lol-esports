152f9cf4ffd6e3288ddd1c311c7b521113944e77
Andrew Marshall <andydanger12@outlook.com>
2026-05-04 14:37:29 -0500

    clutch banner

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,8 @@ import { Navbar } from "./components/Navbar/Navbar";
 import { useTheme } from './theme/ThemeContext'
 import React from "react";
 import ThunderPickBanner from './thunderpick_960x140.png';
+import ClutchBanner from './clutch_banner_2692x826.png';
+import MobileClutchBanner from './clutch_banner_1652x826.png';
 
 function App() {
     const { theme } = useTheme();
@@ -23,8 +25,11 @@ function App() {
                         <Route path="/live/:gameid" component={Match} />
                         <Redirect to="/" />
                     </Switch>
-                    <a className='thunderPickBanner' href="https://go.thunder.partners/visit/?bta=37411&nci=5627&campaign=WELCOME" target="_top">
-                        <img src={ThunderPickBanner} width="100%" height="auto" loading="lazy" />
+                    <a className='desktopClutchBanner' href="https://www.clutch.app/signup?affiliateCode=weedpicks" rel="noopener noreferrer" target="_blank">
+                        <img src={ClutchBanner} width="100%" height="auto" loading="lazy" />
+                    </a>
+                    <a className='mobileClutchBanner' href="https://www.clutch.app/signup?affiliateCode=weedpicks" rel="noopener noreferrer" target="_blank">
+                        <img src={MobileClutchBanner} width="100%" height="auto" loading="lazy" />
                     </a>
                 </div>
                 <Footer />
