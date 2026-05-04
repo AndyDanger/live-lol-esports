93e4e4a29449d83745e9a11af10d284efe1ece5d
Andrew Marshall <andydanger12@outlook.com>
2026-05-04 15:09:07 -0500

    Sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -6,30 +6,33 @@ import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
 import { Footer } from "./components/Footer/Footer";
 import { EventsSchedule } from "./components/Schedule/EventsSchedule";
 import { Navbar } from "./components/Navbar/Navbar";
+import { Sidebar, SidebarProvider } from "./components/Sidebar/Sidebar";
+import { FilterProvider } from "./components/Sidebar/FilterContext";
 import { useTheme } from './theme/ThemeContext'
 import React from "react";
-import ThunderPickBanner from './thunderpick_960x140.png';
 
 function App() {
     const { theme } = useTheme();
 
     return (
-        <HashRouter basename="/">
-            <div className="theme-container" style={{ ...theme as React.CSSProperties }}>
-                <Navbar />
-                <div className="container">
-                    <Switch>
-                        <Route path="/" exact component={EventsSchedule} />
-                        <Route path="/live/:gameid" component={Match} />
-                        <Redirect to="/" />
-                    </Switch>
-                    {/* <a className='thunderPickBanner' href="https://go.thunder.partners/visit/?bta=37411&nci=5627&campaign=WELCOME" target="_top">
-                        <img src={ThunderPickBanner} width="100%" height="auto" loading="lazy" />
-                    </a> */}
-                </div>
-                <Footer />
-            </div>
-        </HashRouter>
+        <FilterProvider>
+            <SidebarProvider>
+                <HashRouter basename="/">
+                    <div className="theme-container" style={{ ...(theme as React.CSSProperties) }}>
+                        <Navbar />
+                        <Sidebar />
+                        <div className="container">
+                            <Switch>
+                                <Route path="/" exact component={EventsSchedule} />
+                                <Route path="/live/:gameid" component={Match} />
+                                <Redirect to="/" />
+                            </Switch>
+                        </div>
+                        <Footer />
+                    </div>
+                </HashRouter>
+            </SidebarProvider>
+        </FilterProvider>
     );
 }
 
