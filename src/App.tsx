aba76badf3e9ee9bb224fc12bd075c7acb09b199
Andrew Marshall <andydanger12@outlook.com>
2026-05-11 20:38:37 -0500

    discord banner

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,8 @@ import { useTheme } from './theme/ThemeContext'
 import React from "react";
 import ClutchBanner from './clutch_banner_2692x826.png';
 import MobileClutchBanner from './clutch_banner_mobile.png';
+import DesktopDiscordBanner from './discord_banner_desktop.png';
+import MobileDiscordBanner from './discord_banner_mobile.png';
 
 function App() {
     const { theme } = useTheme();
@@ -29,11 +31,17 @@ function App() {
                                 <Route path="/live/:gameid" component={Match} />
                                 <Redirect to="/" />
                             </Switch>
-                            <a className='desktopClutchBanner' href="https://www.clutch.app/signup?affiliateCode=weedpicks" rel="noopener noreferrer" target="_blank">
+                            <a className='desktopBanner clutch' href="https://www.clutch.app/signup?affiliateCode=weedpicks" rel="noopener noreferrer" target="_blank">
                               <img src={ClutchBanner} width="100%" height="auto" loading="lazy" />
                             </a>
-                            <a className='mobileClutchBanner' href="https://www.clutch.app/signup?affiliateCode=weedpicks" rel="noopener noreferrer" target="_blank">
+                            <a className='mobileBanner clutch' href="https://www.clutch.app/signup?affiliateCode=weedpicks" rel="noopener noreferrer" target="_blank">
                               <img src={MobileClutchBanner} width="100%" height="auto" loading="lazy" />
+                            </a>
+                            <a className='desktopBanner discord' href="https://discord.gg/tgdGB3MvzG" rel="noopener noreferrer" target="_blank">
+                              <img src={DesktopDiscordBanner} width="100%" height="auto" loading="lazy" />
+                            </a>
+                            <a className='mobileBanner discord' href="https://discord.gg/tgdGB3MvzG" rel="noopener noreferrer" target="_blank">
+                              <img src={MobileDiscordBanner} width="100%" height="auto" loading="lazy" />
                             </a>
                         </div>
                         <Footer />
