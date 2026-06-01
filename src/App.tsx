102597dc679dc4d31d1ec5b12546bf35dc4aa02a
Andrew Marshall <andydanger12@outlook.com>
2026-06-01 10:08:07 -0500

    Remove banners

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,6 @@ import { Sidebar, SidebarProvider } from "./components/Sidebar/Sidebar";
 import { FilterProvider } from "./components/Sidebar/FilterContext";
 import { useTheme } from './theme/ThemeContext'
 import React from "react";
-import ClutchBanner from './clutch_banner_2692x826.png';
-import MobileClutchBanner from './clutch_banner_mobile.png';
-import DesktopDiscordBanner from './discord_banner_desktop.png';
-import MobileDiscordBanner from './discord_banner_mobile.png';
 
 function App() {
     const { theme } = useTheme();
@@ -31,18 +27,6 @@ function App() {
                                 <Route path="/live/:gameid" component={Match} />
                                 <Redirect to="/" />
                             </Switch>
-                            <a className='desktopBanner clutch' href="https://www.clutch.app/signup?affiliateCode=weedpicks" rel="noopener noreferrer" target="_blank">
-                              <img src={ClutchBanner} width="100%" height="auto" loading="lazy" />
-                            </a>
-                            <a className='mobileBanner clutch' href="https://www.clutch.app/signup?affiliateCode=weedpicks" rel="noopener noreferrer" target="_blank">
-                              <img src={MobileClutchBanner} width="100%" height="auto" loading="lazy" />
-                            </a>
-                            <a className='desktopBanner discord' href="https://discord.gg/tgdGB3MvzG" rel="noopener noreferrer" target="_blank">
-                              <img src={DesktopDiscordBanner} width="100%" height="auto" loading="lazy" />
-                            </a>
-                            <a className='mobileBanner discord' href="https://discord.gg/tgdGB3MvzG" rel="noopener noreferrer" target="_blank">
-                              <img src={MobileDiscordBanner} width="100%" height="auto" loading="lazy" />
-                            </a>
                         </div>
                         <Footer />
                     </div>
